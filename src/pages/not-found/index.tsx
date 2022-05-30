aa2f410047d960031927d03d355650719abce887
Carlos Espinoza <carlosarmikhael@gmail.com>
2022-05-30 00:14:45 -0400

    page not-found

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,7 +1,9 @@
 export default function NotFound() {
 	return (
-		<div>
-			<h1>NotFound</h1>
+		<div className='ml-not-found-content'>
+			<img src={`${import.meta.env.VITE_APP_IMG_BBP}/nLH7PqS/404.png`} />
+			<h3>Parece que esta página no existe</h3>
+			<a href='/'>Ir a la página principal</a>
 		</div>
 	)
 }
